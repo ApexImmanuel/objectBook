9a23dfe9aaf5ad9baf0eff0ab6d21882ef035afd
ApexImmanuel <apexbecham@hotmail.com>
2023-05-23 14:16:20 +0200

    saves user input into array

--- main.js
+++ main.js
@@ -16,27 +16,33 @@ closeFormButton.addEventListener("click", closeForm);
 /////////////////////
 /////////////////////
 // Getting users input
+let bookObject = [];
 const submitButton = document.querySelector("#submit");
 submitButton.addEventListener("click", function () {
   let bookTitle = document.querySelector(".inputTitle").value;
   let bookAuthor = document.querySelector(".inputAuthor").value;
   let bookPages = document.querySelector(".inputPages").value;
+  bookObject.push({
+    title: bookTitle,
+    author: bookAuthor,
+    pages: bookPages,
+  });
 });
 
+console.log(bookObject);
 
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//   }
   
-  Book.prototype.info = function () {
-    return (`The ${this.title} is a great book written by ${this.author} and has ${this.pages} pages`);
-  }
-  let bookOne = new Book("One piece", "Oda", 900);
-  console.log(bookOne.title);
-  console.log(bookOne.info());
+//   Book.prototype.info = function () {
+//     return (`The ${this.title} is a great book written by ${this.author} and has ${this.pages} pages`);
+//   }
+//   let bookOne = new Book("One piece", "Oda", 900);
+//   console.log(bookOne.title);
+//   console.log(bookOne.info());
   
   
