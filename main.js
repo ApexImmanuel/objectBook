17b5af20b74230ce33458bb1f5eaca913c14a581
Becham <Apexbecham@hotmail.com>
2023-06-27 09:15:32 +0200

    cleaner codes, functions into smaller parts

--- main.js
+++ main.js
@@ -1,121 +1,111 @@
-  //Form POPUP
-  const addBookButton = document.querySelector("#addBookButton");
-  const closeFormButton = document.querySelector("#closeform");
-  const divPopup = document.querySelector(".formpopup");
+const addBookButton = document.querySelector("#addBookButton");
+const closeFormButton = document.querySelector("#closeform");
+const divPopup = document.querySelector(".formpopup");
+const submitButton = document.querySelector("#submit");
+const gridContainer = document.querySelector(".gridContainer");
 
-  function openForm() {
-    divPopup.style.display = "block";
-    closeFormButton.style.display = "block";
-  }
-  function closeForm() {
-    divPopup.style.display = "none";
-    closeFormButton.style.display = "none";
-  }
-  addBookButton.addEventListener("click", openForm);
-  closeFormButton.addEventListener("click", closeForm);
-  /////////////////////
-  /////////////////////
-  // Getting users input
-  
-  let bookObject = [];
-  const submitButton = document.querySelector("#submit");
-  submitButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    let inputTitle = document.querySelector(".inputTitle");
-    let inputAuthor = document.querySelector(".inputAuthor");
-    let inputPages = document.querySelector(".inputPages");
-    const readSelect  = document.querySelector("select");
-    const readValue = readSelect.value;
-    const bookTitle = inputTitle.value;
-    const bookAuthor = inputAuthor.value;
-    const bookPages = inputPages.value;
-    bookObject.push({
-      title: bookTitle,
-      author: bookAuthor,
-      pages: bookPages,
-      read: readValue,
-    });
-    inputTitle.value = "";
-    inputAuthor.value = "";
-    inputPages.value = "";
-  
-    libraryBook(bookObject);
-    bookObject.shift();
-    closeForm();
-  }); 
-  
+// Event listeners
+addBookButton.addEventListener("click", openForm);
+closeFormButton.addEventListener("click", closeForm);
+submitButton.addEventListener("click", submitForm);
 
-  ///////////////////////////
-  // report cardContainer//
-  function libraryBook(books) {
-    let gridContainer = document.querySelector(".gridContainer");
-    for (const book of books) {
-      const bookCard = document.createElement("div");
-      let spanTitle = document.createElement("span");
-      let spanAuthor = document.createElement("span");
-      let spanPages = document.createElement("span");
-      let spanRead = document.createElement("span");
+// Open the form
+function openForm() {
+  divPopup.style.display = "block";
+  closeFormButton.style.display = "block";
+}
 
-      let deleteButton = document.createElement("button");
-      deleteButton.id = "deleteButton";
-      deleteButton.textContent = "X"
+// Close the form
+function closeForm() {
+  divPopup.style.display = "none";
+  closeFormButton.style.display = "none";
+}
 
-      let readStatusButton = document.createElement("button");
-      readStatusButton.classList.add("statusButton");
-      readStatusButton.textContent= "Change Read Status";
+// Submit the form
+function submitForm(e) {
+  e.preventDefault();
+  const inputTitle = document.querySelector(".inputTitle");
+  const inputAuthor = document.querySelector(".inputAuthor");
+  const inputPages = document.querySelector(".inputPages");
+  const readSelect = document.querySelector("select");
+  const readValue = readSelect.value;
+  const bookTitle = inputTitle.value;
+  const bookAuthor = inputAuthor.value;
+  const bookPages = inputPages.value;
 
-      const pTitle = document.createElement("p");
-      const pAuthor = document.createElement("p");
-      const pPages = document.createElement("p");
-      const pRead = document.createElement("p");
+  const book = {
+    title: bookTitle,
+    author: bookAuthor,
+    pages: bookPages,
+    read: readValue,
+  };
 
-      pTitle.textContent = `Book Title: `;
-      pAuthor.textContent = `Author: `;
-      pPages.textContent = `Pages: `;
-      pRead.textContent = "Have you read it? ";
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
 
-      spanTitle.textContent = `${book.title}`;
-      spanAuthor.textContent = `${book.author}`;
-      spanPages.textContent = `${book.pages}`;
-      spanRead.textContent = `${book.read}`;
+  addBookToLibrary(book);
+  closeForm();
+}
 
-      gridContainer.appendChild(bookCard);
-      bookCard.appendChild(pTitle);
-      bookCard.appendChild(pAuthor);
-      bookCard.appendChild(pPages);
-      bookCard.appendChild(pRead);
-      pTitle.appendChild(spanTitle);
-      pAuthor.appendChild(spanAuthor);
-      pPages.appendChild(spanPages);
-      pRead.appendChild(spanRead);
-      bookCard.appendChild(deleteButton);
-      readStatusButton.addEventListener("click", () => {
-        if (spanRead.textContent == "Yes") {
-          readStatusButton.style.backgroundColor = "red"
-          spanRead.textContent = "No";
-        } else {
-          readStatusButton.style.backgroundColor = "green"
-          spanRead.textContent = "Yes";
-        }
-      })
-      deleteButton.addEventListener("click", () => {
-        bookCard.remove();
-      });
-      bookCard.appendChild(readStatusButton);
+// Add book to library
+function addBookToLibrary(book) {
+  const bookCard = createBookCard(book);
+  gridContainer.appendChild(bookCard);
+}
+
+// Create a book card
+function createBookCard(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("bookCard");
+
+  const deleteButton = createButton("deleteButton", "X");
+  deleteButton.addEventListener("click", () => {
+    bookCard.remove();
+  });
+
+  const readStatusButton = createButton("statusButton", "Change Read Status");
+  readStatusButton.addEventListener("click", () => {
+    const spanRead = bookCard.querySelector(".bookRead");
+    if (spanRead.textContent === "Yes") {
+      readStatusButton.style.backgroundColor = "red";
+      spanRead.textContent = "No";
+    } else {
+      readStatusButton.style.backgroundColor = "green";
+      spanRead.textContent = "Yes";
     }
-  }
+  });
 
-  // function Book(title, author, pages, read) {
-  //     this.title = title;
-  //     this.author = author;
-  //     this.pages = pages;
-  //     this.read = read;
-  //   }
-  //   Book.prototype.info = function () {
-  //     return (`The ${this.title} is a great book written by ${this.author} and has ${this.pages} pages`);
-  //   }
-  //   let bookOne = new Book("One piece", "Oda", 900);
-  //   console.log(bookOne.title);
-  //   console.log(bookOne.info());
-    
-    
+  const pTitle = createParagraph("Book Title: ", "bookTitle", book.title);
+  const pAuthor = createParagraph("Author: ", "bookAuthor", book.author);
+  const pPages = createParagraph("Pages: ", "bookPages", book.pages);
+  const pRead = createParagraph("Have you read it? ", "bookRead", book.read);
+
+  bookCard.appendChild(pTitle);
+  bookCard.appendChild(pAuthor);
+  bookCard.appendChild(pPages);
+  bookCard.appendChild(pRead);
+  bookCard.appendChild(deleteButton);
+  bookCard.appendChild(readStatusButton);
+
+  return bookCard;
+}
+
+// Helper function to create a button
+function createButton(id, text) {
+  const button = document.createElement("button");
+  button.id = id;
+  button.textContent = text;
+  return button;
+}
+
+// Helper function to create a paragraph with a span element
+function createParagraph(label, className, text) {
+  const p = document.createElement("p");
+  const span = document.createElement("span");
+  span.textContent = text;
+  p.textContent = label;
+  p.appendChild(span);
+  p.classList.add(className);
+  return p;
+}
